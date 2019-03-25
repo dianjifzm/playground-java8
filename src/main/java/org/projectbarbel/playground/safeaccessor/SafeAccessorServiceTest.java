4233072a75f65bb94552bbd7f329df7e2efbfb61
Niklas Schlimm <ns103@hotmail.de>
2019-03-25 10:43:58 +0100

    Test case enhancements#

--- src/main/java/org/projectbarbel/playground/safeaccessor/SafeAccessorServiceTest.java
+++ src/main/java/org/projectbarbel/playground/safeaccessor/SafeAccessorServiceTest.java
@@ -6,11 +6,15 @@ import java.util.concurrent.Semaphore;
 
 import org.projectbarbel.playground.testutil.ConcurrentExecutor;
 
+import io.github.benas.randombeans.api.EnhancedRandom;
+
 public class SafeAccessorServiceTest {
 
     public static void main(String[] args) throws InterruptedException {
         
         SafeAccessorService accessor = new SafeAccessorService();
+        
+        System.out.println("\n*** getting an instance ***\n");
         
         ConcurrentExecutor.time(Executors.newFixedThreadPool(4, r -> {
             Thread t = Executors.defaultThreadFactory().newThread(r);
@@ -21,18 +25,37 @@ public class SafeAccessorServiceTest {
             System.out.println("done");
         });
         
+        System.out.println("\n*** randomly updating an instance ***\n");
+
         ConcurrentExecutor.time(Executors.newFixedThreadPool(4, r -> {
             Thread t = Executors.defaultThreadFactory().newThread(r);
             t.setDaemon(true);
             return t;
         }), 4, () -> {
             try {
-                accessor.updateData("test", "data");
+                accessor.updateData("test", EnhancedRandom.random(String.class));
                 System.out.println("done");
             } catch (ConcurrentModificationException e) {
-                System.out.println("failed, handle this - maybe do soemthing else and then try again later");
+                System.out.println("object was locked, handle this and then try again later");
             }
         });
+        
+        System.out.println("\n*** updating an instance with some data ***\n");
+        
+        ConcurrentExecutor.time(Executors.newFixedThreadPool(4, r -> {
+            Thread t = Executors.defaultThreadFactory().newThread(r);
+            t.setDaemon(true);
+            return t;
+        }), 4, () -> {
+            try {
+                accessor.updateData("test", "some data");
+                System.out.println("done");
+            } catch (ConcurrentModificationException e) {
+                System.out.println("object was locked, handle this and then try again later");
+            }
+        });
+        
+        System.out.println("\n*** updating an instance using a semaphore ***\n");
         
         Semaphore semaphore = new Semaphore(1);
         ConcurrentExecutor.time(Executors.newFixedThreadPool(4, r -> {
